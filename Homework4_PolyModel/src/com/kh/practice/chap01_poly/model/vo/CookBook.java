054ca6303b52ed7bad6a69895b17f90da69d8790
ExiNni <sangsa02@naver.com>
2023-09-29 20:28:16 +0900

    practice

--- Homework4_PolyModel/src/com/kh/practice/chap01_poly/model/vo/CookBook.java
+++ Homework4_PolyModel/src/com/kh/practice/chap01_poly/model/vo/CookBook.java
@@ -11,7 +11,7 @@ public class CookBook extends Book {
 	public CookBook(String title, String author, String publisher, boolean coupon) {
 		super(title, author, publisher);
 		this.coupon = coupon;
-	}  
+	}
 
 	public boolean isCoupon() {
 		return coupon;
@@ -20,7 +20,7 @@ public class CookBook extends Book {
 	public void setCoupon(boolean coupon) {
 		this.coupon = coupon;
 	}
-          
+
 	@Override
 	public String toString() {
 		return super.toString() + "\n쿠폰 유무: " + (coupon ? "있음" : "없음");
